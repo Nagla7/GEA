4321515f009c3e681bf11635fef255ff9b173a69
Wejdan11 <32482846+Wejdan11@users.noreply.github.com>
2018-01-31 05:49:14 +0300

    GEA login page

--- GEA/LoginViewController.swift
+++ GEA/LoginViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var UserSegment: UISegmentedControl!
     @IBOutlet weak var UserLable: UILabel!
     @IBOutlet weak var UserName: UITextField!
@@ -17,10 +17,10 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var Password: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func SelectUserTybe(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
         case 0:
@@ -36,15 +36,16 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
