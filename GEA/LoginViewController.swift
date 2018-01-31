e68f8ec7ca66e513997fe41161710af5385c2ee8
Wejdan11 <32482846+Wejdan11@users.noreply.github.com>
2018-01-31 05:42:13 +0300

    GEA Admin login

---
+++ GEA/LoginViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LoginViewController.swift
+//  GEA
+//
+//  Created by user2 on ١٤ جما١، ١٤٣٩ هـ.
+//  Copyright © ١٤٣٩ هـ com.GP.ET. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var UserSegment: UISegmentedControl!
+    @IBOutlet weak var UserLable: UILabel!
+    @IBOutlet weak var UserName: UITextField!
+    @IBOutlet weak var PasswordLable: UILabel!
+    @IBOutlet weak var Password: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func SelectUserTybe(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            print("GEA")
+        case 1:
+            print("Admin")
+        default:
+            print("none")
+        }
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GEATests/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  GEATests
+//
+//  Created by user2 on ١٤ جما١، ١٤٣٩ هـ.
+//  Copyright © ١٤٣٩ هـ com.GP.ET. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var UserName: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    @IBOutlet weak var UserSegment: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func SelectUserTybe(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            print("GEA")
+     case 1:
+            print("Admin")
+        default:
+            print("Defult")
+        }
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
