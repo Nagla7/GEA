db19f3c83bbe16a204b4adad1dbb58df9eb05fb5
leena hassan <leena@leenas-macbook-pro.local>
2018-02-08 00:50:55 +0300

    publish reg pop up

---
+++ GEA/Popup.swift
@@ -0,0 +1,25 @@
+//
+//  Popup.swift
+//  GEA
+//
+//  Created by leena hassan on 22/05/1439 AH.
+//  Copyright © 1439 com.GP.ET. All rights reserved.
+//
+
+import UIKit
+
+class Popup: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
+
